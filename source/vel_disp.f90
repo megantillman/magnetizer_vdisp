2fd3d8aca84d0f97a9ec315b94aac4813fe65596
Megan Tillman <40581182+megantillman@users.noreply.github.com>
2026-04-15 15:05:27 -0400

    Update vel_disp.f90
    more precision fixes

--- source/vel_disp.f90
+++ source/vel_disp.f90
@@ -24,8 +24,8 @@ module vel_disp
   double precision, parameter :: phiQ=2.0
   
   double precision, parameter :: pc=3.0856776d18
-  double precision, parameter :: yr=365.25*24.*3600.
-  double precision, parameter :: Myr=1e6*yr
+  double precision, parameter :: yr=365.25d0*24.0d0*3600.0d0
+  double precision, parameter :: Myr=1.0d6*yr
   double precision, parameter :: kmps = 1d5
   double precision, parameter :: G=6.6743d-8
   double precision, parameter :: Msun=1.9891d33
@@ -37,7 +37,7 @@ module vel_disp
 contains 
   subroutine calc_v_kms(Mgas_disk, r_disk, v_disk, SFR, p_ISM_sound_speed_km_s, v_kms)
 
-    ! function inputs from the main code
+    ! subroutine inputs from the main code
     double precision, intent(in) :: p_ISM_sound_speed_km_s
     double precision, intent(in) :: Mgas_disk, r_disk, v_disk, SFR 
 
@@ -47,7 +47,7 @@ contains
     double precision, dimension(nx) :: sigma_g, sigma_sf
     double precision :: maxfac_g
     
-    ! function output, v_kms is an input to be consistent with the main code but is not affected by what is actually given to the subroutine
+    ! subroutine output, v_kms is an input to be consistent with the main code but is not affected by what is actually given to the subroutine
     double precision, dimension(nx), intent(out) :: v_kms
     
     ! calc orbital time in Myrs
@@ -72,7 +72,7 @@ contains
     call calc_sig_sf(p_ISM_sound_speed_km_s, sigma_sf, t_orb, tsfmax)
     
     ! calc approximately the gas surface density to see which turb vel to use
-    Sig_sf = sqrt(2.0d0*(1.0d0+beta)) * (2.0d0*pi/(t_orb*Myr)) * sigma_sf * kmps / (pi*G*(Q/fgQ)) / (Msun/pc**2.0d0)
+    Sig_sf = sqrt(2.0d0*(1.0d0+beta)) * (2.0d0*pi/(t_orb*Myr)) * sigma_sf * kmps / (pi*G*(Q/fgQ)) / (Msun/pc**2)
     
     ! check if we can support via SF or through SF and mass transport
     if (Sig_g(nx) .gt. Sig_sf(nx) .and. sigma_g(1) .gt. sigma_sf(1)) then
@@ -98,28 +98,32 @@ contains
   
   
   subroutine calc_sig_sf(p_ISM_sound_speed_km_s, sigma_sf, t_orb, tsfmax)
-    
+
+    ! subroutine inputs
+    double precision, intent(in) :: p_ISM_sound_speed_km_s
+    double precision, intent(in) :: t_orb, tsfmax
+
+    ! local variables
     double precision :: fac1, fac
     double precision :: a3, a2, a1, a0
     double precision :: refpoint
     double precision :: sigma_sf2
     double precision, dimension(3) :: roots
-    double precision, intent(in) :: p_ISM_sound_speed_km_s
-    double precision, intent(in) :: t_orb, tsfmax
-    
+
+    !subroutine output
     double precision, dimension(nx), intent(out):: sigma_sf
     
-    fac1=sqrt(3.0*fgP/(8.0*(1.0+beta)))*Q*phimp/(4*fgQ*epsff)*t_orb/tsfmax
-    if (fac1 .lt. 1.0) then
-      fac1=1.0
+    fac1=sqrt(3.0d0*fgP/(8.0d0*(1.0d0+beta)))*Q*phimp/(4*fgQ*epsff)*t_orb/tsfmax
+    if (fac1 .lt. 1.0d0) then
+      fac1=1.0d0
     endif
-    fac=4.0*fac1*fsf*epsff*pmstar/(sqrt(3.0*fgP)*pi*eta*phimp*phiQ)
+    fac=4.0d0*fac1*fsf*epsff*pmstar/(sqrt(3.0d0*fgP)*pi*eta*phimp*phiQ)
     
-    a3=1.0
-    a2=-(3.0*p_ISM_sound_speed_km_s**2+fac**2)
-    a1=3.0*p_ISM_sound_speed_km_s**4
+    a3=1.0d0
+    a2=-(3.0d0*p_ISM_sound_speed_km_s**2+fac**2)
+    a1=3.0d0*p_ISM_sound_speed_km_s**4
     a0=-p_ISM_sound_speed_km_s**6
-    refpoint = 230.
+    refpoint = 230.0d0
   
     sigma_sf2 = CubicRootClose(a3, a2, a1, a0, refpoint, roots)
     sigma_sf = sqrt(sigma_sf2)
